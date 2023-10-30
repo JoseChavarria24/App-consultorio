096fda58fee6522a42f78d5618dea47ef7dfe3b0
Dell <Dell@DESKTOP-KTML4LO>
2023-10-29 23:46:51 -0600

    Se cambiaron los TextFields del panel citas por los personalizados y se modifico la etiqueta del panel oeste 2

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -80,7 +80,7 @@ public class Menu extends JFrame {
         lbl_titulo = new JLabel(" CONSULTORIO ");
         lbl_oeste0 = new JLabel(" REGISTRO ");
         lbl_oeste1 = new JLabel(" PACIENTE ");
-        lbl_oeste2 = new JLabel("--");
+        lbl_oeste2 = new JLabel(" CITAS ");
         lbl_oeste3 = new JLabel("--");
         lbl_oeste4 = new JLabel(" SALIR ");
         lbl_logo = new JLabel(new ImageIcon("Logo.png"));
